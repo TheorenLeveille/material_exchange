2b3ad0b19aecd3e58bbc42f07a9bcd69c3235b92
Theoren Leveille <leveilletheoren@gmail.com>
2019-04-06 19:10:48 -0700

    finished upload material

--- js/upload_material.js
+++ js/upload_material.js
@@ -1,9 +1,28 @@
 function uploadMaterial(){
+    let photo = document.getElementById('photo').value;
+    let category = document.getElementById('categories').value;
     let materialType = document.getElementById('mType').value;
     let quantity = document.getElementById('qname').value;
     let description = document.getElementById('description').value;
-    let photo = 
+    let contact = document.getElementById('contact').value;
+   
+    // Strips file path and file extension so that it can be used as a key in our database
+    photo = photo.replace(/\.[^/.]+$/, "");
+    photo = photo.replace(/^.*[\\\/]/, '');
+   
+    
+    console.log(photo);
+    console.log(category)
     console.log(materialType);
     console.log(quantity);
     console.log(description);
+    console.log(contact);
+
+    firebase.database().ref("materials/"+ category + "/" + photo + "/").update(
+		{
+        "contact":contact,
+        "description":description,
+        "material type":materialType,
+        "quantity":quantity
+        });
 }
